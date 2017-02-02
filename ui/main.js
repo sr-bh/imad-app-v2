3ffb7ff2d244fab8065b4e303f35a3e1d1ddec93
sr-bh <srimoyeebhattacharyya@gmail.com>
2017-02-02 22:48:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,16 @@
 console.log('Loaded!');
 
+var button = document.getElementById('counter');
+var counter=0;
 
-var img=document.getElementById('madi');
-var marginLeft=0
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-
-img.onclick=function(){
-    var interval= setInterval(moveRight,100);
+button.onclick = function(){
+    
+    //
+    
+    //
+    
+    //Render variable in the correct span
+    counter=counter+1;
+    var span= getElementById('count');
+    span.innerHTML=counter.toString();
 };
