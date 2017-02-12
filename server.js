2f76da9ec9fd6b270ecb0be0b5f5baa1100d5c0f
sr-bh <srimoyeebhattacharyya@gmail.com>
2017-02-12 23:46:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,7 +97,7 @@ app.get('/', function (req, res) {
 app.get('/:articleName', function(req,res){
     //articleName == article-one
     //articles[articleName] == {} content object for article one
-    var articleNmae = req.params.articleName;
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
 });
 
