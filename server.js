9a3c309de6a95cb844381571cbeac122b816f993
sr-bh <srimoyeebhattacharyya@gmail.com>
2017-03-25 16:22:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+var names = [];
+app.get('/submit-name', function(req,res){
+    var name = req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/articles/:articleName', function(req,res){
     //articleName == article-one
     //articles[articleName] == {} content object for article one
